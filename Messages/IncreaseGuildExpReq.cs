cddb1c06d8af7cabc392da519e9a19f586d44abb
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-06 05:55:27 +0700

    Revert "feat: change increase guild exp request message"
    This reverts commit eb3bfd4eb9284486b0717036fac361c7c618a76a.

--- Messages/IncreaseGuildExpReq.cs
+++ Messages/IncreaseGuildExpReq.cs
@@ -3,8 +3,6 @@
     public partial struct IncreaseGuildExpReq
     {
         public int GuildId { get; set; }
-        public int Level { get; set; }
         public int Exp { get; set; }
-        public int SkillPoint { get; set; }
     }
 }
