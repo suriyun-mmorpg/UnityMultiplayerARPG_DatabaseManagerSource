eb3bfd4eb9284486b0717036fac361c7c618a76a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-06 05:38:52 +0700

    feat: change increase guild exp request message

--- Messages/IncreaseGuildExpReq.cs
+++ Messages/IncreaseGuildExpReq.cs
@@ -3,6 +3,8 @@
     public partial struct IncreaseGuildExpReq
     {
         public int GuildId { get; set; }
+        public int Level { get; set; }
         public int Exp { get; set; }
+        public int SkillPoint { get; set; }
     }
 }
