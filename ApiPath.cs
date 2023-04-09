aae5c844eab1a5415ece32b98aa159749153013b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-10 04:12:20 +0700

    feat: Add api path consts

---
+++ ApiPath.cs
@@ -0,0 +1,76 @@
+﻿namespace MultiplayerARPG.MMO
+{
+    public class DatabaseApiPath
+    {
+        public const string ValidateUserLogin = "validate-user-login";
+        public const string ValidateAccessToken = "validate-access-token";
+        public const string GetUserLevel = "get-user-level";
+        public const string GetGold = "get-gold";
+        public const string ChangeGold = "change-gold";
+        public const string GetCash = "get-cash";
+        public const string ChangeCash = "change-cash";
+        public const string UpdateAccessToken = "update-access-token";
+        public const string CreateUserLogin = "create-user-login";
+        public const string FindUsername = "find-username";
+        public const string CreateCharacter = "create-character";
+        public const string ReadCharacter = "read-character";
+        public const string ReadCharacters = "read-characters";
+        public const string UpdateCharacter = "update-character";
+        public const string DeleteCharacter = "delete-character";
+        public const string FindCharacterName = "find-character-name";
+        public const string FindCharacters = "find-characters";
+        public const string CreateFriend = "create-friend";
+        public const string DeleteFriend = "delete-friend";
+        public const string ReadFriends = "read-friends";
+        public const string CreateBuilding = "create-building";
+        public const string UpdateBuilding = "update-building";
+        public const string DeleteBuilding = "delete-building";
+        public const string ReadBuildings = "read-buildings";
+        public const string CreateParty = "create-party";
+        public const string UpdateParty = "update-party";
+        public const string UpdatePartyLeader = "update-party-leader";
+        public const string DeleteParty = "delete-party";
+        public const string UpdateCharacterParty = "update-character-party";
+        public const string ClearCharacterParty = "clear-character-party";
+        public const string ReadParty = "read-party";
+        public const string CreateGuild = "create-guild";
+        public const string UpdateGuildLeader = "update-guild-leader";
+        public const string UpdateGuildMessage = "update-guild-message";
+        public const string UpdateGuildRole = "update-guild-role";
+        public const string UpdateGuildMemberRole = "update-guild-member-role";
+        public const string DeleteGuild = "delete-guild";
+        public const string UpdateCharacterGuild = "update-character-guild";
+        public const string ClearCharacterGuild = "clear-character-guild";
+        public const string FindGuildName = "find-guild-name";
+        public const string ReadGuild = "read-guild";
+        public const string IncreaseGuildExp = "increase-guild-exp";
+        public const string AddGuildSkill = "add-guild-skill";
+        public const string GetGuildGold = "get-guild-gold";
+        public const string ChangeGuildGold = "change-guild-gold";
+        public const string ReadStorageItems = "read-storage-items";
+        public const string UpdateStorageItems = "update-storage-items";
+        public const string MailList = "mail-list";
+        public const string UpdateReadMailState = "update-read-mail-state";
+        public const string UpdateClaimMailItemsState = "update-claim-mail-items-state";
+        public const string UpdateDeleteMailState = "update-delete-mail-state";
+        public const string SendMail = "send-mail";
+        public const string GetMail = "get-mail";
+        public const string GetIdByCharacterName = "get-id-by-character-name";
+        public const string GetUserIdByCharacterName = "get-user-id-by-character-name";
+        public const string UpdateGuildMessage2 = "update-guild-message-2";
+        public const string UpdateGuildScore = "update-guild-score";
+        public const string UpdateGuildOptions = "update-guild-options";
+        public const string UpdateGuildAutoAcceptRequests = "update-guild-auto-accept-requests";
+        public const string UpdateGuildRank = "update-guild-rank";
+        public const string GetMailNotification = "get-mail-notification";
+        public const string GetUserUnbanTime = "get-user-unban-time";
+        public const string SetUserUnbanTimeByCharacterName = "set-user-unban-time-by-character-name";
+        public const string SetCharacterUnmuteTimeByName = "set-character-unmute-time-by-name";
+        public const string GetSummonBuffs = "get-summon-buffs";
+        public const string SetSummonBuffs = "set-summon-buffs";
+        public const string ValidateEmailVerification = "validate-email-verification";
+        public const string FindEmail = "find-email";
+        public const string GetFriendRequestNotification = "get-friend-request-notification";
+        public const string UpdateUserCount = "update-user-count";
+    }
+}
