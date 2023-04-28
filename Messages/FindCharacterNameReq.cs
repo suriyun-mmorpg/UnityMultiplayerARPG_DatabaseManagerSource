1143cc95b30a17d766324acc54be5b0cbe69ccf8
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-28 12:36:05 +0700

    feat: allow some field to be empty

--- Messages/FindCharacterNameReq.cs
+++ Messages/FindCharacterNameReq.cs
@@ -4,7 +4,7 @@
     public partial struct FindCharacterNameReq
     {
         public string? FinderId { get; set; }
-        public string? CharacterName { get; set; }
+        public string CharacterName { get; set; }
         public int Skip { get; set; }
         public int Limit { get; set; }
     }
