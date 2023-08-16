d858b928ef51a76d72f8c9788d8bfd715e656ec3
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-08-16 13:46:03 +0700

    feat: WIP storage items saving structure changes

--- Messages/UpdateBuildingReq.cs
+++ Messages/UpdateBuildingReq.cs
@@ -1,10 +1,26 @@
-﻿namespace MultiplayerARPG.MMO
+﻿using System.Collections.Generic;
+
+namespace MultiplayerARPG.MMO
 {
 #nullable enable
     public partial struct UpdateBuildingReq
     {
+        [System.Flags]
+        public enum EExtraContent
+        {
+            None = 0,
+            StorageItems = 1 << 0,
+        }
+
         public string ChannelId { get; set; }
         public string MapName { get; set; }
         public BuildingSaveData BuildingData { get; set; }
+        public EExtraContent ExtraContent { get; set; }
+        public List<CharacterItem> StorageItems { get; set; }
+
+        public bool HasExtraContent(EExtraContent flag)
+        {
+            return (ExtraContent & flag) == flag;
+        }
     }
 }
--- Messages/UpdateCharacterReq.cs
+++ Messages/UpdateCharacterReq.cs
@@ -1,8 +1,26 @@
-﻿namespace MultiplayerARPG.MMO
+﻿using System.Collections.Generic;
+
+namespace MultiplayerARPG.MMO
 {
 #nullable enable
     public partial struct UpdateCharacterReq
     {
+        [System.Flags]
+        public enum EExtraContent
+        {
+            None = 0,
+            StorageItems = 1 << 0,
+            SummonBuffs = 1 << 0,
+        }
+
         public PlayerCharacterData CharacterData { get; set; }
+        public EExtraContent ExtraContent { get; set; }
+        public List<CharacterItem> StorageItems { get; set; }
+        public List<CharacterBuff> SummonBuffs { get; set; }
+
+        public bool HasExtraContent(EExtraContent flag)
+        {
+            return (ExtraContent & flag) == flag;
+        }
     }
 }
