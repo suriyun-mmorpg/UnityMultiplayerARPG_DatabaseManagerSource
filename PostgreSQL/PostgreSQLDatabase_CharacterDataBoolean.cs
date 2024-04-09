f6b8e07823fed8ddddf9507b87890121ca63159a
insthync <ittipon.bay@gmail.com>
2024-04-09 17:12:40 +0700

    feat: add function for character data booeans filling

--- PostgreSQL/PostgreSQLDatabase_CharacterDataBoolean.cs
+++ PostgreSQL/PostgreSQLDatabase_CharacterDataBoolean.cs
@@ -1,61 +1,39 @@
 #if NET || NETCOREAPP
-using Cysharp.Text;
 using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
 using Npgsql;
+using NpgsqlTypes;
 using System.Collections.Generic;
 
 namespace MultiplayerARPG.MMO
 {
     public partial class PostgreSQLDatabase
     {
-        private bool ReadCharacterDataBoolean(NpgsqlDataReader reader, out CharacterDataBoolean result)
+        public const string CACHE_KEY_FILL_CHARACTER_DATA_BOOLEANS_UPDATE = "FILL_CHARACTER_DATA_BOOLEANS_UPDATE";
+        public const string CACHE_KEY_FILL_CHARACTER_DATA_BOOLEANS_INSERT = "FILL_CHARACTER_DATA_BOOLEANS_INSERT";
+        public async UniTask FillCharacterDataBooleans(NpgsqlConnection connection, NpgsqlTransaction transaction, string tableName, string characterId, IList<CharacterDataBoolean> characterDataBooleans)
         {
-            if (reader.Read())
-            {
-                result = new CharacterDataBoolean();
-                result.hashedKey = reader.GetInt32(0);
-                result.value = reader.GetBoolean(1);
-                return true;
-            }
-            result = default;
-            return false;
-        }
-
-        public async UniTask CreateCharacterDataBoolean(NpgsqlConnection connection, NpgsqlTransaction transaction, string tableName, HashSet<string> insertedIds, string characterId, CharacterDataBoolean characterDataBoolean)
-        {
-            string id = ZString.Concat(characterId, "_", characterDataBoolean.hashedKey);
-            if (insertedIds.Contains(id))
-            {
-                LogWarning(LogTag, $"Custom Boolean {id}, for character {characterId}, already inserted to table {tableName}");
-                return;
-            }
-            insertedIds.Add(id);
-            await ExecuteNonQuery(connection, transaction, $"INSERT INTO {tableName} (id, characterId, hashedKey, value) VALUES (@id, @characterId, @hashedKey, @value)",
-                new NpgsqlParameter("@id", id),
-                new NpgsqlParameter("@characterId", characterId),
-                new NpgsqlParameter("@hashedKey", characterDataBoolean.hashedKey),
-                new NpgsqlParameter("@value", characterDataBoolean.value));
-        }
-
-        public async UniTask<List<CharacterDataBoolean>> ReadCharacterDataBooleans(string tableName, string characterId, List<CharacterDataBoolean> result = null)
-        {
-            if (result == null)
-                result = new List<CharacterDataBoolean>();
-            await ExecuteReader((reader) =>
-            {
-                CharacterDataBoolean tempData;
-                while (ReadCharacterDataBoolean(reader, out tempData))
+            int count = await PostgreSQLHelpers.ExecuteUpdate(
+                $"{CACHE_KEY_FILL_CHARACTER_DATA_BOOLEANS_UPDATE}_{tableName}",
+                connection, transaction,
+                tableName,
+                new[]
                 {
-                    result.Add(tempData);
-                }
-            }, $"SELECT hashedKey, value FROM {tableName} WHERE characterId=@characterId",
-                new NpgsqlParameter("@characterId", characterId));
-            return result;
-        }
-
-        public async UniTask DeleteCharacterDataBooleans(NpgsqlConnection connection, NpgsqlTransaction transaction, string tableName, string characterId)
-        {
-            await ExecuteNonQuery(connection, transaction, $"DELETE FROM {tableName} WHERE characterId=@characterId", new NpgsqlParameter("@characterId", characterId));
+                    new PostgreSQLHelpers.ColumnInfo(NpgsqlDbType.Jsonb, "data", JsonConvert.SerializeObject(characterDataBooleans)),
+                },
+                new[]
+                {
+                    PostgreSQLHelpers.WhereEqualTo("id", characterId),
+                });
+            if (count <= 0)
+            {
+                await PostgreSQLHelpers.ExecuteInsert(
+                    $"{CACHE_KEY_FILL_CHARACTER_DATA_BOOLEANS_INSERT}_{tableName}",
+                    connection, null,
+                    tableName,
+                    new PostgreSQLHelpers.ColumnInfo("id", characterId),
+                    new PostgreSQLHelpers.ColumnInfo(NpgsqlDbType.Jsonb, "data", JsonConvert.SerializeObject(characterDataBooleans)));
+            }
         }
     }
 }
