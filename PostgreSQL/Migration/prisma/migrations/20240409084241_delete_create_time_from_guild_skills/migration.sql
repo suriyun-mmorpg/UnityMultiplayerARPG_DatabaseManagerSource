d156aae0912b0a95e586781f021e3d5f7b090a42
insthync <ittipon.bay@gmail.com>
2024-04-09 15:42:57 +0700

    feat: delete create time from guild skills

---
+++ PostgreSQL/Migration/prisma/migrations/20240409084241_delete_create_time_from_guild_skills/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `create_time` on the `guild_skills` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "guild_skills" DROP COLUMN "create_time";
