189c51b65be043eed2e85dc4afa2e9eef4c278ef
insthync <ittipon.bay@gmail.com>
2024-04-09 17:12:02 +0700

    feat: add function for character quests filling

--- PostgreSQL/PostgreSQLDatabase_CharacterQuest.cs
+++ PostgreSQL/PostgreSQLDatabase_CharacterQuest.cs
@@ -1,71 +1,39 @@
 ﻿#if NET || NETCOREAPP
-using Cysharp.Text;
 using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
 using Npgsql;
+using NpgsqlTypes;
 using System.Collections.Generic;
 
 namespace MultiplayerARPG.MMO
 {
     public partial class PostgreSQLDatabase
     {
-        private bool ReadCharacterQuest(NpgsqlDataReader reader, out CharacterQuest result)
+        public const string CACHE_KEY_FILL_CHARACTER_QUESTS_UPDATE = "FILL_CHARACTER_QUESTS_UPDATE";
+        public const string CACHE_KEY_FILL_CHARACTER_QUESTS_INSERT = "FILL_CHARACTER_QUESTS_INSERT";
+        public async UniTask FillCharacterQuests(NpgsqlConnection connection, NpgsqlTransaction transaction, string characterId, IList<CharacterQuest> characterQuests)
         {
-            if (reader.Read())
-            {
-                result = new CharacterQuest();
-                result.dataId = reader.GetInt32(0);
-                result.randomTasksIndex = reader.GetByte(1);
-                result.isComplete = reader.GetBoolean(2);
-                result.completeTime = reader.GetInt64(3);
-                result.isTracking = reader.GetBoolean(4);
-                result.ReadKilledMonsters(reader.GetString(5));
-                result.ReadCompletedTasks(reader.GetString(6));
-                return true;
-            }
-            result = CharacterQuest.Empty;
-            return false;
-        }
-
-        public async UniTask CreateCharacterQuest(NpgsqlConnection connection, NpgsqlTransaction transaction, HashSet<string> insertedIds, string characterId, CharacterQuest characterQuest)
-        {
-            string id = ZString.Concat(characterId, "_", characterQuest.dataId);
-            if (insertedIds.Contains(id))
-            {
-                LogWarning(LogTag, $"Quest {id}, for character {characterId}, already inserted");
-                return;
-            }
-            insertedIds.Add(id);
-            await ExecuteNonQuery(connection, transaction, "INSERT INTO characterquest (id, characterId, dataId, randomTasksIndex, isComplete, completeTime, isTracking, killedMonsters, completedTasks) VALUES (@id, @characterId, @dataId, @randomTasksIndex, @isComplete, @completeTime, @isTracking, @killedMonsters, @completedTasks)",
-                new NpgsqlParameter("@id", id),
-                new NpgsqlParameter("@characterId", characterId),
-                new NpgsqlParameter("@dataId", characterQuest.dataId),
-                new NpgsqlParameter("@randomTasksIndex", characterQuest.randomTasksIndex),
-                new NpgsqlParameter("@isComplete", characterQuest.isComplete),
-                new NpgsqlParameter("@completeTime", characterQuest.completeTime),
-                new NpgsqlParameter("@isTracking", characterQuest.isTracking),
-                new NpgsqlParameter("@killedMonsters", characterQuest.WriteKilledMonsters()),
-                new NpgsqlParameter("@completedTasks", characterQuest.WriteCompletedTasks()));
-        }
-
-        public async UniTask<List<CharacterQuest>> ReadCharacterQuests(string characterId, List<CharacterQuest> result = null)
-        {
-            if (result == null)
-                result = new List<CharacterQuest>();
-            await ExecuteReader((reader) =>
-            {
-                CharacterQuest tempQuest;
-                while (ReadCharacterQuest(reader, out tempQuest))
+            int count = await PostgreSQLHelpers.ExecuteUpdate(
+                CACHE_KEY_FILL_CHARACTER_QUESTS_UPDATE,
+                connection, transaction,
+                "character_quests",
+                new[]
                 {
-                    result.Add(tempQuest);
-                }
-            }, "SELECT dataId, randomTasksIndex, isComplete, completeTime, isTracking, killedMonsters, completedTasks FROM characterquest WHERE characterId=@characterId ORDER BY id ASC",
-                new NpgsqlParameter("@characterId", characterId));
-            return result;
-        }
-
-        public async UniTask DeleteCharacterQuests(NpgsqlConnection connection, NpgsqlTransaction transaction, string characterId)
-        {
-            await ExecuteNonQuery(connection, transaction, "DELETE FROM characterquest WHERE characterId=@characterId", new NpgsqlParameter("@characterId", characterId));
+                    new PostgreSQLHelpers.ColumnInfo(NpgsqlDbType.Jsonb, "data", JsonConvert.SerializeObject(characterQuests)),
+                },
+                new[]
+                {
+                    PostgreSQLHelpers.WhereEqualTo("id", characterId),
+                });
+            if (count <= 0)
+            {
+                await PostgreSQLHelpers.ExecuteInsert(
+                    CACHE_KEY_FILL_CHARACTER_QUESTS_INSERT,
+                    connection, null,
+                    "character_quests",
+                    new PostgreSQLHelpers.ColumnInfo("id", characterId),
+                    new PostgreSQLHelpers.ColumnInfo(NpgsqlDbType.Jsonb, "data", JsonConvert.SerializeObject(characterQuests)));
+            }
         }
     }
 }
