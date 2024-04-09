4b1bfb8b6ee707d15b322c18654b4b375ece3774
insthync <ittipon.bay@gmail.com>
2024-04-09 15:39:18 +0700

    feat: WIP rework on postgresql implementation (party)

--- PostgreSQL/PostgreSQLDatabase_Party.cs
+++ PostgreSQL/PostgreSQLDatabase_Party.cs
@@ -1,96 +1,169 @@
 ﻿#if NET || NETCOREAPP
 using Cysharp.Threading.Tasks;
-using Npgsql;
 
 namespace MultiplayerARPG.MMO
 {
     public partial class PostgreSQLDatabase
     {
+        public const string CACHE_KEY_INSERT_PARTY = "INSERT_PARTY";
+        public const string CACHE_KEY_INSERT_PARTY_UPDATE = "INSERT_PARTY_UPDATE";
         public override async UniTask<int> CreateParty(bool shareExp, bool shareItem, string leaderId)
         {
+            using var connection = await _dataSource.OpenConnectionAsync();
+            using var transaction = await connection.BeginTransactionAsync();
             int id = 0;
-            await ExecuteReader((reader) =>
+            try
             {
-                if (reader.Read())
-                    id = reader.GetInt32(0);
-            }, "INSERT INTO party (shareExp, shareItem, leaderId) VALUES (@shareExp, @shareItem, @leaderId);" +
-                "SELECT LAST_INSERT_ID();",
-                new NpgsqlParameter("@shareExp", shareExp),
-                new NpgsqlParameter("@shareItem", shareItem),
-                new NpgsqlParameter("@leaderId", leaderId));
-            if (id > 0)
+                id = (int)await PostgreSQLHelpers.ExecuteInsertScalar(
+                    CACHE_KEY_INSERT_PARTY,
+                    connection, transaction,
+                    "parties",
+                    new[] {
+                        new PostgreSQLHelpers.ColumnInfo("share_exp", shareExp),
+                        new PostgreSQLHelpers.ColumnInfo("share_item", shareItem),
+                        new PostgreSQLHelpers.ColumnInfo("leader_id", leaderId),
+                    }, "id");
+                if (id > 0)
+                {
+                    await PostgreSQLHelpers.ExecuteUpdate(
+                        CACHE_KEY_INSERT_PARTY_UPDATE,
+                        connection, transaction,
+                        "characters",
+                        new[]
+                        {
+                            new PostgreSQLHelpers.ColumnInfo("party_id", id),
+                        },
+                        PostgreSQLHelpers.WhereEqualTo("id", leaderId));
+                }
+                await transaction.CommitAsync();
+            }
+            catch (System.Exception ex)
             {
-                await ExecuteNonQuery("UPDATE characters SET partyId=@id WHERE id=@leaderId",
-                    new NpgsqlParameter("@id", id),
-                    new NpgsqlParameter("@leaderId", leaderId));
+                LogError(LogTag, "Transaction, Error occurs while create party: " + id);
+                LogException(LogTag, ex);
+                await transaction.RollbackAsync();
             }
             return id;
         }
 
+        public const string CACHE_KEY_READ_PARTY_PARTIES = "READ_PARTY_PARTIES";
+        public const string CACHE_KEY_READ_PARTY_MEMBERS = "READ_PARTY_MEMBERS";
         public override async UniTask<PartyData> ReadParty(int id)
         {
+            using var connection = await _dataSource.OpenConnectionAsync();
+            using var readerParties = await PostgreSQLHelpers.ExecuteSelect(
+                CACHE_KEY_READ_PARTY_PARTIES,
+                connection, null,
+                "parties", "share_exp, share_item, leader_id", "LIMIT 1",
+                PostgreSQLHelpers.WhereEqualTo("id", id));
             PartyData result = null;
-            await ExecuteReader((reader) =>
+            if (readerParties.Read())
             {
-                if (reader.Read())
-                {
-                    result = new PartyData(id,
-                        reader.GetBoolean(0),
-                        reader.GetBoolean(1),
-                        reader.GetString(2));
-                }
-            }, "SELECT shareExp, shareItem, leaderId FROM party WHERE id=@id LIMIT 1",
-                new NpgsqlParameter("@id", id));
+                result = new PartyData(id,
+                    readerParties.GetBoolean(0),
+                    readerParties.GetBoolean(1),
+                    readerParties.GetString(2));
+            }
             // Read relates data if party exists
             if (result != null)
             {
                 // Party members
-                await ExecuteReader((reader) =>
+                using var readerMembers = await PostgreSQLHelpers.ExecuteSelect(
+                    CACHE_KEY_READ_PARTY_MEMBERS,
+                    connection, null,
+                    "characters", "id, data_id, character_name, level",
+                    PostgreSQLHelpers.WhereEqualTo("party_id", id));
+                SocialCharacterData partyMemberData;
+                while (readerMembers.Read())
                 {
-                    SocialCharacterData partyMemberData;
-                    while (reader.Read())
-                    {
-                        // Get some required data, other data will be set at server side
-                        partyMemberData = new SocialCharacterData();
-                        partyMemberData.id = reader.GetString(0);
-                        partyMemberData.dataId = reader.GetInt32(1);
-                        partyMemberData.characterName = reader.GetString(2);
-                        partyMemberData.level = reader.GetInt32(3);
-                        result.AddMember(partyMemberData);
-                    }
-                }, "SELECT id, dataId, characterName, level FROM characters WHERE partyId=@id",
-                    new NpgsqlParameter("@id", id));
+                    // Get some required data, other data will be set at server side
+                    partyMemberData = new SocialCharacterData();
+                    partyMemberData.id = readerMembers.GetString(0);
+                    partyMemberData.dataId = readerMembers.GetInt32(1);
+                    partyMemberData.characterName = readerMembers.GetString(2);
+                    partyMemberData.level = readerMembers.GetInt32(3);
+                    result.AddMember(partyMemberData);
+                }
             }
             return result;
         }
 
+        public const string CACHE_KEY_UPDATE_PARTY_LEADER = "UPDATE_PARTY_LEADER";
         public override async UniTask UpdatePartyLeader(int id, string leaderId)
         {
-            await ExecuteNonQuery("UPDATE party SET leaderId=@leaderId WHERE id=@id",
-                new NpgsqlParameter("@leaderId", leaderId),
-                new NpgsqlParameter("@id", id));
+            using var connection = await _dataSource.OpenConnectionAsync();
+            await PostgreSQLHelpers.ExecuteUpdate(
+                CACHE_KEY_UPDATE_PARTY_LEADER,
+                connection, null,
+                "parties",
+                new[]
+                {
+                    new PostgreSQLHelpers.ColumnInfo("leader_id", leaderId),
+                },
+                PostgreSQLHelpers.WhereEqualTo("id", id));
         }
 
+        public const string CACHE_KEY_UPDATE_PARTY = "UPDATE_PARTY";
         public override async UniTask UpdateParty(int id, bool shareExp, bool shareItem)
         {
-            await ExecuteNonQuery("UPDATE party SET shareExp=@shareExp, shareItem=@shareItem WHERE id=@id",
-                new NpgsqlParameter("@shareExp", shareExp),
-                new NpgsqlParameter("@shareItem", shareItem),
-                new NpgsqlParameter("@id", id));
+            using var connection = await _dataSource.OpenConnectionAsync();
+            await PostgreSQLHelpers.ExecuteUpdate(
+                CACHE_KEY_UPDATE_PARTY,
+                connection, null,
+                "parties",
+                new[]
+                {
+                    new PostgreSQLHelpers.ColumnInfo("share_exp", shareExp),
+                    new PostgreSQLHelpers.ColumnInfo("share_item", shareItem),
+                },
+                PostgreSQLHelpers.WhereEqualTo("id", id));
         }
 
+        public const string CACHE_KEY_DELETE_PARTY_PARTY = "DELETE_PARTY_PARTY";
+        public const string CACHE_KEY_DELETE_PARTY_CHARACTER = "DELETE_PARTY_CHARACTER";
         public override async UniTask DeleteParty(int id)
         {
-            await ExecuteNonQuery("DELETE FROM party WHERE id=@id;" +
-                "UPDATE characters SET partyId=0 WHERE partyId=@id;",
-                new NpgsqlParameter("@id", id));
+            using var connection = await _dataSource.OpenConnectionAsync();
+            using var transaction = await connection.BeginTransactionAsync();
+            try
+            {
+                await PostgreSQLHelpers.ExecuteDelete(
+                    CACHE_KEY_DELETE_PARTY_PARTY,
+                    connection, transaction,
+                    "parties",
+                    PostgreSQLHelpers.WhereEqualTo("id", id));
+                await PostgreSQLHelpers.ExecuteUpdate(
+                    CACHE_KEY_DELETE_PARTY_CHARACTER,
+                    connection, transaction,
+                    "characters",
+                    new[]
+                    {
+                        new PostgreSQLHelpers.ColumnInfo("party_id", 0),
+                    },
+                    PostgreSQLHelpers.WhereEqualTo("party_id", id));
+                await transaction.CommitAsync();
+            }
+            catch (System.Exception ex)
+            {
+                LogError(LogTag, "Transaction, Error occurs while delete party: " + id);
+                LogException(LogTag, ex);
+                await transaction.RollbackAsync();
+            }
         }
 
+        public const string CACHE_KEY_UPDATE_CHARACTER_PARTY = "UPDATE_CHARACTER_PARTY";
         public override async UniTask UpdateCharacterParty(string characterId, int partyId)
         {
-            await ExecuteNonQuery("UPDATE characters SET partyId=@partyId WHERE id=@characterId",
-                new NpgsqlParameter("@characterId", characterId),
-                new NpgsqlParameter("@partyId", partyId));
+            using var connection = await _dataSource.OpenConnectionAsync();
+            await PostgreSQLHelpers.ExecuteUpdate(
+                CACHE_KEY_UPDATE_CHARACTER_PARTY,
+                connection, null,
+                "characters",
+                new[]
+                {
+                    new PostgreSQLHelpers.ColumnInfo("party_id", partyId),
+                },
+                PostgreSQLHelpers.WhereEqualTo("id", characterId));
         }
     }
 }
