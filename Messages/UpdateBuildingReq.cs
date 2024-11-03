e44ca140b260db74eab3981e4358e9a526149e5d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2024-11-04 02:31:31 +0700

    feat: WIP partially transactional character updating

--- Messages/UpdateBuildingReq.cs
+++ Messages/UpdateBuildingReq.cs
@@ -5,6 +5,7 @@ namespace MultiplayerARPG.MMO
 #nullable enable
     public partial struct UpdateBuildingReq
     {
+        public TransactionUpdateBuildingState State { get; set; }
         public string ChannelId { get; set; }
         public string MapName { get; set; }
         public BuildingSaveData BuildingData { get; set; }
